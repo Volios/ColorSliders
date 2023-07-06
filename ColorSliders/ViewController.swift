f30288b8b571532eba1cb7cb45d0ac4c84e06c7f
Volios <gilbertbytes@gmail.com>
2023-07-06 18:33:21 +0300

    Final fix errors and mistakes

--- ColorSliders/ViewController.swift
+++ ColorSliders/ViewController.swift
@@ -20,38 +20,52 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-            
         colorView.layer.cornerRadius = 16
-        
         setupSliders()
-        
-        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
-                                            green: CGFloat(greenSlider.value),
-                                            blue: CGFloat(blueSlider.value),
-                                            alpha: 1)
-        
+        setColor()
+        setValue()
     }
     
     // MARK: - IBActions
     @IBAction func changeColorSlider(_ sender: UISlider) {
-        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
-                                            green: CGFloat(greenSlider.value),
-                                            blue: CGFloat(blueSlider.value),
-                                            alpha: 1)
-        
-        redValueLabel.text = String(format: "%.2f", redSlider.value)
-        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
-        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        setColor()
+        switch sender {
+        case redSlider:
+            redValueLabel.text = string(from: sender)
+        case greenSlider:
+            greenValueLabel.text = string(from: sender)
+        default:
+            blueValueLabel.text = string(from: sender)
+        }
     }
     
     // MARK: - Private Methods
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setValue() {
+        redValueLabel.text = string(from: redSlider)
+        greenValueLabel.text = string(from: greenSlider)
+        blueValueLabel.text = string(from: blueSlider)
+    }
+    
     private func setupSliders() {
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
         blueSlider.minimumTrackTintColor = .blue
     }
-    
+        
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
 }
 
